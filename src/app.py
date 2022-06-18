f3a94845f3a4253d52e2456de2e669ec165050b8
juliopm <juliopm@gmail.com>
2022-06-18 20:12:38 -0300

    adicao do lixo xd

--- src/app.py
+++ src/app.py
@@ -2,18 +2,6 @@ from flask import app, Flask, render_template
 
 minha_app = Flask(__name__)
 
-# @minha_app.route('/')
-# def home():
-#     return render_template('home.html')
-
-
-# @minha_app.route('/personalizada')
-# def home_perso():
-#     usu = 'Fabrício'
-#     return render_template('home_personalizada.html', usuario=usu)
-
-
-
 @minha_app.route("/")
 def home():
     return render_template("home.html")
