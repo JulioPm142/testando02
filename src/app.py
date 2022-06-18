cc5d52dfd431cc5f5fa7bc82cbadcd8a9b471f60
juliopm <juliopm@gmail.com>
2022-06-18 20:05:19 -0300

    first commit

---
+++ src/app.py
@@ -0,0 +1,37 @@
+from flask import app, Flask, render_template
+
+minha_app = Flask(__name__)
+
+# @minha_app.route('/')
+# def home():
+#     return render_template('home.html')
+
+
+# @minha_app.route('/personalizada')
+# def home_perso():
+#     usu = 'Fabrício'
+#     return render_template('home_personalizada.html', usuario=usu)
+
+
+
+@minha_app.route("/")
+def home():
+    return render_template("home.html")
+
+@minha_app.route("/artes")
+def artes():
+    return render_template("Artes.html")
+
+@minha_app.route("/Break-In-Time")
+def BIT():
+    return render_template("Break_in_Time.html")
+
+@minha_app.route("/Projeto Integrador")
+def API():
+    return render_template("Projeto_Integrador.html")
+
+
+
+
+if __name__ == '__main__':
+    minha_app.run('0.0.0.0')
